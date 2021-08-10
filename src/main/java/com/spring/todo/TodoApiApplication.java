1fb84bd6e3680ce1573c4b585b801839e5d6c149
Gleison Mozart <mozart.gleison@gmail.com>
2021-08-10 00:05:16 -0300

    Repository Criado para o Inject

--- src/main/java/com/spring/todo/TodoApiApplication.java
+++ src/main/java/com/spring/todo/TodoApiApplication.java
@@ -1,13 +1,29 @@
 package com.spring.todo;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.spring.todo.domain.Todo;
+
 @SpringBootApplication
-public class TodoApiApplication {
+public class TodoApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		
+		Todo t1 = new Todo(null,"Estudar","Estudar Spring Boot 2 e Angular 11", LocalDateTime.parse("09/08/2021 23:47", formatter), false);
+		
+	
 	}
 
 }
