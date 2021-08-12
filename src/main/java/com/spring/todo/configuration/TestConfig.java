393c503173cbea19bdd2f6986d1cc249e88d8c02
Gleison Mozart <mozart.gleison@gmail.com>
2021-08-11 23:41:21 -0300

    Instanciação de forma automatizada

--- src/main/java/com/spring/todo/configuration/TestConfig.java
+++ src/main/java/com/spring/todo/configuration/TestConfig.java
@@ -1,6 +1,7 @@
 package com.spring.todo.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -15,7 +16,7 @@ public class TestConfig {
 	@Autowired
 	private TodoService  tdService;
 	
-	
+	@Bean
 	public boolean instancia() {
 		
 		this.tdService.instnaciaBaseDeDados();
